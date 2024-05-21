bec06f0818dba1889af54910899268057e9122cb
emi.abd <abdalaemiiliano@gmail.com>
2024-05-21 09:54:56 -0300

    soluciones tareas

--- Clase.11/TAREA js/3/script.js
+++ Clase.11/TAREA js/3/script.js
@@ -1,12 +1,12 @@
-let A = prompt('Por favor introduzca la longitud del lado A del triangulo: ')
-let B = prompt('Por favor introduzca la longitud del lado B del triangulo: ')
-let C = prompt('Por favor introduzca la longitud del lado C del triangulo: ')
+let A = Number(prompt('Por favor introduzca la longitud del lado A del triangulo: '))
+let B = Number(prompt('Por favor introduzca la longitud del lado B del triangulo: '))
+let C = Number(prompt('Por favor introduzca la longitud del lado C del triangulo: '))
 
 if(A<=0 || B<=0 || C<=0){
     console.log('Longitudes no validas')
-}else if(A==B && B==C){
+}else if(A===B && B===C){
     console.log('El triangulo formado es un triangulo Equilatero')
-}else if(A==B || B==C || A==C){
+}else if(A===B || B===C || A===C){
     console.log('El triangulo formado es un triangulo Isosceles')
 }else{
     console.log('El triangulo formado es un triangulo Escaleno')
--- Clase.11/TAREA js/5/script.js
+++ Clase.11/TAREA js/5/script.js
@@ -1,10 +1,20 @@
-let temp = Number(prompt('Ingrese una temperatura en grados Celsius: '))
-if (temp<0){
+let temp = prompt('Ingrese una temperatura en grados Celsius: ')
+
+/* if (temp === '' || temp === null || isNaN(temp)){
+    alert('Temperatura no valida')
+}else if (temp<1){
     console.log('Solido')
 }else if(temp<=99){
     console.log('Liquido')
 }else if(temp>=100){
     console.log('Gaseoso')
-}else{
+} */
+if (!temp || isNaN(temp)){
     alert('Temperatura no valida')
-}
+}else if (temp<1){
+    console.log('Solido')
+}else if(temp<=99){
+    console.log('Liquido')
+}else if(temp>=100){
+    console.log('Gaseoso')
+} 
