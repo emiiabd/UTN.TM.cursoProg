38ea6d4d597030ef6b0e5509016ab3113a61f82e
emiiabd <abdalaemiliano@gmail.com>
2024-07-02 16:34:13 -0300

    Tarea clase.22

---
+++ Clase.22/src/components/dataCards/dataCards.js
@@ -0,0 +1,66 @@
+const cards =[
+  {
+    colors: ['#1A6D2E','#4F6F52','#E8DFCA','#F5EFE6'],
+    likes: '5,452',
+    date: '17 Hours'
+  },
+  {
+    colors: ['#124123','#4F6F52','#E8DFCA','#F5EFE6'],
+    likes: '10,452',
+    date: '3 Hours'
+  },
+  {
+    colors: ['#487798','#4F6F52','#E8DFCA','#F5EFE6'],
+    likes: '11,500',
+    date: '18 Hours'
+  },
+  {
+    colors: ['#1A4365','#4F6F52','#E8DFCA','#F5EFE6'],
+    likes: '152',
+    date: '36 Hours'
+  },
+  ///////
+  {
+    colors: ['#1A6D2E','#4F6da2','#E8D23A','#F5EFE6'],
+    likes: '5,452',
+    date: '17 Hours'
+  },
+  {
+    colors: ['#124123','#4F6F52','#E8DFCA','#F5dFE6'],
+    likes: '89,452',
+    date: '5 Hours'
+  },
+  {
+    colors: ['#222639','#114F52','#EaaFCA','#F532E6'],
+    likes: '500',
+    date: '2 Hours'
+  },
+  {
+    colors: ['#1AA365','#426F52','#E8DFCA','#F5EFE6'],
+    likes: '1,252',
+    date: '30 Hours'
+  },
+  /////
+  {
+    colors: ['#189D2E','#4F6F52','#E23FCA','#F5EFE6'],
+    likes: '5,452',
+    date: '11 Hours'
+  },
+  {
+    colors: ['#1BG523','#4F6F52','#E8DFCA','#aaEFE6'],
+    likes: '10,002',
+    date: '1 Hours'
+  },
+  {
+    colors: ['#487798','#4F6F52','#E8DFCA','#F23FE6'],
+    likes: '115,500',
+    date: '8 Hours'
+  },
+  {
+    colors: ['#1A4365','#4F6F52','#E8DFCA','#F5EFE6'],
+    likes: '333',
+    date: '26 Hours'
+  },
+];
+
+export default cards;
